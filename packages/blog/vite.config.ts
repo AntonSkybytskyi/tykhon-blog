74193a92a18d588be174b8e582c0bb7670ad1608
Anton Skybytskyi <a.skybytskyi@gmail.com>
2023-04-09 16:38:19 +0300

    Remove netlify depenency, try vercel

--- packages/blog/vite.config.ts
+++ packages/blog/vite.config.ts
@@ -1,10 +1,10 @@
-import netlify from "solid-start-netlify";
+import vercel from "solid-start-vercel";
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [solid({
-    adapter: netlify({ edge: true })
+    adapter: vercel({})
   })],
   ssr: { external: ["@prisma/client"] },
 });
