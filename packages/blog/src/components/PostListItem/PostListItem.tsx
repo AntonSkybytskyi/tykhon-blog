bea4c818257079cb88085c1fba20df29b90734b5
Anton Skybytskyi <a.skybytskyi@gmail.com>
2023-04-10 23:39:09 +0300

    Remove hover form post title on list view`

--- packages/blog/src/components/PostListItem/PostListItem.tsx
+++ packages/blog/src/components/PostListItem/PostListItem.tsx
@@ -7,7 +7,7 @@ const PostListItem: Component<IPostListItem> = ({ title, description, slug, thum
         <A href={`/posts/${slug}`} class="block bg-white rounded-lg shadow border border-transparent hover:border-blue-500 ">
             {thumbnail && <img src={thumbnail} alt={title} class="w-full h-64 object-cover rounded-t-lg" />}
             <div class="p-4 align-bottom justify-end">
-                <h3 class="block text-lg font-semibold mb-2 hover:text-blue-500">{title}</h3>
+                <h3 class="block text-lg font-semibold mb-2">{title}</h3>
                 <p class="text-gray-600 leading-relaxed mb-4">{description}</p>
             </div>
         </A>
