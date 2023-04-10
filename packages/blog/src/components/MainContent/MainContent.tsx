6f747fe1ee2c9fa0356f91795b265e7f15848a5f
Anton Skybytskyi <a.skybytskyi@gmail.com>
2023-04-10 20:28:34 +0300

    Add loading state to main conent

--- packages/blog/src/components/MainContent/MainContent.tsx
+++ packages/blog/src/components/MainContent/MainContent.tsx
@@ -1,12 +1,16 @@
-import { Component, JSX } from "solid-js";
+import { Component, JSX, createEffect } from "solid-js";
+import { useIsRouting } from "solid-start";
 
 interface MainContentProps {
     children: JSX.Element;
 }
 const MainContent: Component<MainContentProps> = ({ children }) => {
+    const isRouting = useIsRouting();
+
     return (
         <main class="container mx-auto px-4 my-8 flex-grow">
             {children}
+            {isRouting() && <div class="fixed z-50 inset-0 bg-white opacity-75 flex justify-center items-center" />}
         </main>
     )
 };
