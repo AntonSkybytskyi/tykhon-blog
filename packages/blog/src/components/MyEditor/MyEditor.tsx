47543929d3e1e7116bcf8b86f7181e10224db659
Anton Skybytskyi <a.skybytskyi@gmail.com>
2023-04-15 00:08:04 +0300

    Apply css rule to next line

--- packages/blog/src/components/MyEditor/MyEditor.tsx
+++ packages/blog/src/components/MyEditor/MyEditor.tsx
@@ -119,38 +119,6 @@ function BlockquoteIcon(props: JSX.IntrinsicElements["svg"]): JSX.Element {
     );
 }
 
-const CONTENT = `
-<h2>
-Hi there,
-</h2>
-<p>
-this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-</p>
-<ul>
-<li>
-  That’s a bullet list with one …
-</li>
-<li>
-  … or two list items.
-</li>
-</ul>
-<p>
-Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-</p>
-<pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-<p>
-I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-</p>
-<blockquote>
-Wow, that’s amazing. Good work, boy! 👏
-<br />
-— Mom
-</blockquote>    
-`;
-
-
 function Separator() {
     return (
         <div class="flex items-center" aria-hidden="true">
@@ -321,8 +289,6 @@ function ToolbarContents(props: ToolbarProps): JSX.Element {
 const MyEditor: Component<{ content: string, onUpdate: (html: string) => void }> = ({ content, onUpdate }) => {
     const [container, setContainer] = createSignal<HTMLDivElement>();
     const [menu, setMenu] = createSignal<HTMLDivElement>();
-
-
 
     const editor = createTiptapEditor(() => ({
         element: container()!,
