7ace7ed5ffa9458bb8da3a0bf0075b626df39202
Anton Skybytskyi <a.skybytskyi@gmail.com>
2023-06-18 12:51:24 +0300

    Remove login button from header

--- packages/blog/src/components/Header/Header.tsx
+++ packages/blog/src/components/Header/Header.tsx
@@ -1,14 +1,6 @@
-import { Component, Show, createEffect, createSignal } from "solid-js";
+import { Component, createEffect, createSignal } from "solid-js";
 import { A, useLocation } from "solid-start";
-import { createServerAction$, createServerData$ } from "solid-start/server";
-import { logout } from "~/db/session";
-import Button from "../Button/Button";
 
-export const routeData = () => {
-    return createServerData$(async (_, { request }) => {
-        return {};
-    });
-};
 const Header: Component = () => {
     const [isMenuOpened, setIsMenuOpened] = createSignal(false);
     const location = useLocation();
@@ -19,10 +11,6 @@ const Header: Component = () => {
 
     createEffect(() => {
         location.pathname && setIsMenuOpened(false);
-    });
-
-    const [, { Form }] = createServerAction$((_: FormData) => {
-        return logout("/");
     });
 
     return (
@@ -64,20 +52,6 @@ const Header: Component = () => {
                     >
                         About
                     </A>
-                    <Show when={true}>
-                        <A
-                            href={"/posts"}
-                            activeClass="font-semibold"
-                            class="text-gray-500 hover:text-gray-600 px-4"
-                        >
-                            My Posts
-                        </A>
-                        <Form>
-                            <Button type="submit" class="font-bold">
-                                Logout
-                            </Button>
-                        </Form>
-                    </Show>
                 </div>
             </div>
 
@@ -98,20 +72,6 @@ const Header: Component = () => {
                     >
                         About
                     </A>
-                    <Show when={true}>
-                        <A
-                            href={"/posts"}
-                            activeClass="font-semibold"
-                            class="text-gray-500 hover:text-gray-600 block px-3 py-2 rounded-md text-base"
-                        >
-                            My Posts
-                        </A>
-                        <Form class="px-3">
-                            <Button type="submit" class="font-bold w-full">
-                                Logout
-                            </Button>
-                        </Form>
-                    </Show>
                 </div>
             </div>
         </nav>
